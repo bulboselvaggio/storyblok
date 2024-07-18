c27508af2a1c24c01c5e1091e712b00dbf22f408
bulboselvaggio <bulboselvaggio@gmail.com>
2024-07-18 19:29:14 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
       },
       components: {
         page: 'storyblok/Page',
+        product: 'components/Product',
         config: 'storyblok/Config',
         feature: 'storyblok/Feature',
         grid: 'storyblok/Grid',

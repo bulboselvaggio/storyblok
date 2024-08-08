db55366fe4777d79e6b40bb7eb3b5d5175106611
blacktrend <blacktrend@gmail.com>
2024-08-08 13:43:40 +0200

    seo titles

--- src/seoDefaults.js
+++ src/seoDefaults.js
@@ -1,14 +1,13 @@
 // src/seoDefaults.js
 
 const seoDefaults = {
-  title: 'Comics and tees by Milfo',
-  description: 'Discover the best comics and t-shirts designed by Milfo.',
-  keywords: 'comics, t-shirts, Milfo, art, design',
+  title: 'Comics and tees by Milfoski',
+  description: 'Discover the best comics and t-shirts designed by Milfoski.',
+  keywords: 'comics, t-shirts, Milfoski, art, design',
   ogImage: 'https://a.storyblok.com/f/297215/1280x628/60abf16c8e/milfoski-sharing.png',
   twitterImage: 'https://a.storyblok.com/f/297215/1280x628/60abf16c8e/milfoski-sharing.png',
+  url: 'https://milfoski.com',
   ogType: 'website',
-  pageUrl: 'https://milfo.it/',
-  canonicalUrl: 'https://milfo.it/'
-}
+};
 
-export default seoDefaults
+export default seoDefaults;

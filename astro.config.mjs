f6ed197bf8cfb404cfb2abcc8cfaf459857fd59f
bulboselvaggio <bulboselvaggio@gmail.com>
2024-11-09 20:39:28 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config'
 import storyblok from '@storyblok/astro'
 import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
-import mkcert from 'vite-plugin-mkcert';
+import mkcert from 'vite-plugin-mkcert'
+import sitemap from '@astrojs/sitemap'
+
 const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://milfoski.com',
   integrations: [
     storyblok({
-      //accessToken: env.STORYBLOK_TOKEN,
       accessToken: 'Z25UexdLwCf2QLAZGrP7rwtt',
       apiOptions: {
         region: '',
@@ -37,6 +39,7 @@ export default defineConfig({
       },
     }),
     tailwind(),
+    sitemap(),
   ],
   vite: {
     plugins: [mkcert()],
